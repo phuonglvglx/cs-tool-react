26badebbb7311b7a656aea0df0000eb29eaca43b
phuonglvglx <phuonglv@galaxy.com.vn>
2021-09-29 08:42:04 +0700

    s

--- src/services/request.ts
+++ src/services/request.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
-import { navigate } from '@reach/router';
+import { navigate } from "@reach/router";
+import { message } from "antd";
 
 axios.defaults.baseURL = process.env.API || "https://api.dev.icankids.com.vn";
 axios.interceptors.response.use(
@@ -9,23 +10,26 @@ axios.interceptors.response.use(
   (error) => {
     switch (error.response.status) {
       case 401:
-        localStorage.removeItem('token')
-        navigate('/login');
+        localStorage.removeItem("token");
+        navigate("/login");
         break;
       case 404:
       case 403:
-        navigate('/');
+        navigate("/");
         break;
+      case 500:
+        message.error({content: "500 Internal Server Error"})
+        console.log(500);
     }
     return Promise.reject(error.response);
-  },
+  }
 );
 
 export function setToken(token: string | null) {
   if (token) {
-    axios.defaults.headers.common['Authorization'] = `Token ${token}`;
+    axios.defaults.headers.common["Authorization"] = `Token ${token}`;
   } else {
-    delete axios.defaults.headers.common['Authorization'];
+    delete axios.defaults.headers.common["Authorization"];
   }
 }
 
